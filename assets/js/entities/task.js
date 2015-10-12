f796e8de38ccd9d945aadbcbf6cbebb9909a27b1
Morgan Miller <m.miller722@gmail.com>
2015-10-12 16:17:47 -0600

    refactor task data to request response handler

--- assets/js/entities/task.js
+++ assets/js/entities/task.js
@@ -9,4 +9,27 @@ TaskApp.module("Entities", function(Entities, TaskApp, Backbone, Marionette, $, 
   Entities.TaskCollection = Backbone.Collection.extend({
     model: Entities.Task
   });
+
+  var tasks;
+
+  var initializeTasks = function(){
+    tasks = new Entities.TaskCollection([
+      {id: 1, title: "Learn Marionette!"},
+      {id: 2, title: "Make a task app"},
+      {id: 3, title: "Remember to eat"}
+    ]);
+  };
+
+  var API = {
+    getTaskEntities: function(){
+      if(tasks === undefined){
+        initializeTasks();
+      }
+      return tasks;
+    }
+  };
+
+  TaskApp.reqres.setHandler("task:entities", function(){
+    return API.getTaskEntities();
+  })
 });
